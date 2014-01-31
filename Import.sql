23e1614de8f99e8766f2badd53f138f7b2356ec7
Remi-C <Remi-C@fake.fake>
2014-01-31 10:50:21 +0100

    creating structure

---
+++ Import.sql
@@ -0,0 +1,27 @@
+﻿/*
+*Rémi-C , 2014
+*
+*Script to load Open Data Paris Data into a PostGis data base, taking care of the encoding and the srid
+*
+*/
+
+
+-- Getting the correct srid : you need to get the IGNF srid, see other repository
+--https://github.com/Remi-C/IGN_spatial_ref_for_PostGIS/blob/master/Put_IGN_SRS_into_Postgis.sql
+
+	--do you have the ODParis srid?
+	SELECT *
+	FROM spatial_ref_sys
+	WHERE proj4text ILIKE '%IGNF:LAMB1%';
+
+	--the srid is for "LAMB1" : 932001
+
+--importing one file at a time
+--here are the shell command, for memory : 
+
+	--	arbres_alignement-2010\arbres.shp , LATIN1 
+	--	arbres_remarquables_2011\Arbres_remarquables.shp , LATIN1
+	--	
+
+	SELECT DISTINCT libelle FROM public.barriere    LIMIT 100
+	
---
+++ importFromZipToPostGIS.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Script by Rémi C
+#loading open data paris in postgis
+
+#This script load all the open data paris data in a folder (one zip per data) into a postgis database
